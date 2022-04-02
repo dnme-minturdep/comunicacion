0504ca365eb659161e641794b2d15610c245b331
Pablo Tiscornia <pablotisco@gmail.com>
2022-04-02 09:51:21 -0600

    Sumo funcion scale_color_dnmye

---
+++ R/scale_color_dnmye.R
@@ -0,0 +1,26 @@
+#' Constructor de escala de colores de contorno (color) para la dnmye
+#'
+#' @param palette Nombre de la paleta de colores en dnmye_paleta
+#' @param discrete TRUE indica que la variable es discreta
+#' @param reverse *TRUE* indica que la peleta debe ser invertida
+#' @param ... Argumentos adicionales para definir en discrete_scale() o
+#'            scale_color_gradientn(), respectivamente, cuando discrete es TRUE o FALSE
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
+#' geom_point(size = 4) +
+#' scale_color_dnmye()
+
+
+scale_color_dnmye <- function(palette = "c10_todos", discrete = TRUE, reverse = FALSE, ...) {
+
+  pal <- comunicacion::dnmye_paletas(palette = palette, reverse = reverse)
+
+  if (discrete) {
+    ggplot2::discrete_scale("color", paste0("dnmye_", palette), palette = pal, ...)
+  } else {
+    ggplot2::scale_color_gradientn(colors = pal(256), ...)
+  }
+}
