5369294865b6d5c040bfe8617df60590286f5d41
Pablo Tiscornia <pablotisco@gmail.com>
2022-05-23 15:48:09 -0500

    agrego fuente a zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname){
 
-  sysfonts::font_add_google("Roboto", "Encode Sans Normal")
+  sysfonts::font_add_google("Roboto", "Roboto")
+  sysfonts::font_add_google("Encode Sans", "Encode Sans Normal")
   showtext::showtext_auto()
 
 }
