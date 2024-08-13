35e6916209f2f40f26c30044ddafbd11e743d9e1
eliansoutu <eliansoutul@gmail.com>
2024-08-13 15:39:42 -0300

    actualizo paquete

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
 
-  sysfonts::font_add_google("Roboto", "Roboto")
-  sysfonts::font_add_google("Encode Sans", "Encode Sans Normal")
+  sysfonts::font_add_google("Montserrat", "Montserrat")
+  sysfonts::font_add_google("Lora", "Lora")
 }
